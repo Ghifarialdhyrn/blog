25185545bf46727c262c2a227eab7fcff5dd58ce
gifarialdhynr@gmail.com <gifarialdhynr@gmail.com>
2024-12-17 06:37:11 +0700

    fix

--- next.config.ts
+++ next.config.ts
@@ -6,4 +6,11 @@ const nextConfig: NextConfig = {
   },
 };
 
+module.exports = {
+  env: {
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+  },
+};
+
 export default nextConfig;
